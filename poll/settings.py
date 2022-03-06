7af9013b721c90f987241122d014219907b83cf2
srijanmalhotra2711 <86052891+srijanmalhotra2711@users.noreply.github.com>
2022-03-05 19:00:46 -0500

    Updates

--- poll/settings.py
+++ poll/settings.py
@@ -123,5 +123,5 @@ STATIC_URL = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_on_heroku.settings(locals())
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+django_on_heroku.settings(locals(), test_runner=False)
