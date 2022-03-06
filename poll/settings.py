31f475a1a3afc7685c61fab00d216c5ef3135cac
srijanmalhotra2711 <86052891+srijanmalhotra2711@users.noreply.github.com>
2022-03-05 19:09:01 -0500

    Updates

--- poll/settings.py
+++ poll/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-from re import TEMPLATE
 import django_on_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
